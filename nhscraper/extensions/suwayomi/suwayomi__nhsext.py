718fe37797de0c027dcf763dae06fb8792c452a6
C7YPT0N1C <peaceolawuyi01@gmail.com>
2025-09-08 23:33:25 +0100

    Fixed GraphQL

--- nhscraper/extensions/suwayomi/suwayomi__nhsext.py
+++ nhscraper/extensions/suwayomi/suwayomi__nhsext.py
@@ -357,7 +357,7 @@ def store_creator_manga_IDs(meta: dict):
             logger.debug(f"GraphQL: Looking up manga for creator '{creator_name}' in source {LOCAL_SOURCE_ID}")
             result = graphql_request(query, {
                 "title": creator_name,
-                "sourceId": str(LOCAL_SOURCE_ID)
+                "sourceId": LOCAL_SOURCE_ID
             })
             
             logger.debug(f"GraphQL: Manga lookup result for '{creator_name}': {result}")
@@ -410,7 +410,7 @@ def retry_deferred_creators():
             """
             result = graphql_request(query, {
                 "title": creator_name,
-                "sourceId": str(LOCAL_SOURCE_ID)
+                "sourceId": LOCAL_SOURCE_ID
             })
             logger.debug(f"GraphQL: Retry result for '{creator_name}': {result}")
             nodes = result.get("data", {}).get("mangas", {}).get("nodes", []) if result else []
