84db94a3fe5c57443421342484ca6443cadda9c4
C7YPT0N1C <75539121+C7YPT0N1C@users.noreply.github.com>
2025-09-21 02:13:20 +0100

    General fixes

--- nhscraper/core/downloader.py
+++ nhscraper/core/downloader.py
@@ -363,6 +363,6 @@ def start_downloader(gallery_list=None):
     minutes, seconds = divmod(rem, 60)
     human_runtime = f"{int(hours)}h {int(minutes)}m {seconds:.2f}s" if hours else f"{int(minutes)}m {seconds:.2f}s" if minutes else f"{seconds:.2f}s"
 
-    update_skipped_galleries(True)
+    #update_skipped_galleries(True) # Report all skipped galleries at end
     log_clarification()
     logger.info(f"All ({len(gallery_ids)}) Galleries Processed In {human_runtime}.")
