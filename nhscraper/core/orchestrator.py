f9f0c3a1ff17d96ee876ec339522ec62b363e0f3
C7YPT0N1C <75539121+C7YPT0N1C@users.noreply.github.com>
2025-09-29 19:25:45 +0100

    General fixes

--- nhscraper/core/orchestrator.py
+++ nhscraper/core/orchestrator.py
@@ -543,7 +543,8 @@ def fetch_env_vars():
         min_sleep = get_var("min_sleep")
         max_sleep = get_var("max_sleep")
 
-        use_tor = get_var("use_tor", False)
+        #use_tor = get_var("use_tor", False)
+        use_tor = config.get("USE_TOR", False)
         skip_post_run = get_var("skip_post_run")
         dry_run = get_var("dry_run")
         calm = get_var("calm")
