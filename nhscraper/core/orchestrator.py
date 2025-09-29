5c7fb4ccd6288d854e25773fd87c93f9b25e6b04
C7YPT0N1C <75539121+C7YPT0N1C@users.noreply.github.com>
2025-09-29 11:19:22 +0100

    General fixes

--- nhscraper/core/orchestrator.py
+++ nhscraper/core/orchestrator.py
@@ -385,6 +385,7 @@ def fetch_env_vars():
 
         for key in config.keys():
             globals()[key.lower()] = normalise_value(key, config[key])
+            log(f"{globals()[key.lower()]} = {normalise_value(key, config[key])}")
 
     # Execute the update under the lock
     with_env_lock(_update_globals)
