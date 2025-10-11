011ac046faaf85986af6df1cf92500249738f8d5
C7YPT0N1C <75539121+C7YPT0N1C@users.noreply.github.com>
2025-10-11 13:41:40 +0100

    General fixes

--- nhscraper/core/orchestrator.py
+++ nhscraper/core/orchestrator.py
@@ -431,10 +431,10 @@ def normalise_value(key: str, value):
         return int(value)
     
     # Ensure variables values are valid (non zero / negative)
-    if key.lower() == "threads_galleries":
+    if key.lower() == "threads_galleries" or key.lower() == "threads_images":
         threads_galleries = min(max(MIN_THREADS_GALLERIES, threads_galleries), MAX_THREADS_GALLERIES)
-    if key.lower() == "threads_images":
-        threads_images = min(max(MIN_THREADS_IMAGES, threads_images), MAX_THREADS_IMAGES)
+        calculated_threads_images = round(((MAX_ALLOWED_API_HITS / BATCH_SIZE) - threads_galleries) / threads_galleries)
+        threads_images = min(max(MIN_THREADS_IMAGES, calculated_threads_images), MAX_THREADS_IMAGES)
     if key.lower() == "max_retries":
         max_retries = max(0.1, max_retries)
     min_retry_sleep = max(0.1, min_retry_sleep)
