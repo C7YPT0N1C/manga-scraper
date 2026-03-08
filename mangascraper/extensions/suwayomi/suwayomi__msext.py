1dfb30e15595f66ce018593183c239ac623f42e9
C7YPT0N1C <peaceolawuyi01@gmail.com>
2026-03-08 18:42:04 +0000

    refactor: Add debug logging for image downloads and remove consolidated database update call in after_completed_gallery_download_hook

--- mangascraper/extensions/suwayomi/suwayomi__msext.py
+++ mangascraper/extensions/suwayomi/suwayomi__msext.py
@@ -1001,6 +1001,8 @@ def download_images_hook(gallery, page, urls, path, downloader_session, pbar=Non
     Tries mirrors in order until one succeeds, with retries per mirror.
     Updates tqdm progress bar with current creator.
     """
+    
+    logger.debug("DOWNLOADING IMAGES")
 
     orchestrator.refresh_globals()
     
@@ -1165,22 +1167,6 @@ def after_completed_gallery_download_hook(meta: dict, gallery_id):
         creators = [sanitise_string(c) for c in gallery_meta.get("creator", [])]
         tags = gallery_meta.get("tags", [])
         languages = gallery_meta.get("languages", [])
-        
-        logger.debug("TESTING")
-        # --- Consolidated database update call ---
-        scraperapi.update_gallery_metadata(
-            gallery_id=gallery_id,
-            raw_title=gallery_meta.get("raw_title"),
-            clean_title=gallery_meta.get("clean_title"),
-            language=languages,
-            tags=tags,
-            cover_path=gallery_meta.get("cover_path"),
-            creator_name=creators,
-            download_path=gallery_meta.get("download_path"),
-            extension_used=gallery_meta.get("extension_used"),
-            num_pages=gallery_meta.get("num_pages")
-        )
-
 
         cover_source = None
         cover_gallery_name = None
