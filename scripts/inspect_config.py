3f5d370d98cf564179395851549d12ccdf6b38d8
C7YPT0N1C <peaceolawuyi01@gmail.com>
2026-04-11 16:48:07 +0100

    feat: Remove deprecated scraper service and related scripts

--- scripts/inspect_config.py
+++
@@ -1,6 +0,0 @@
-from mangascraper.core import orchestrator
-from mangascraper.core.api import api as scraperapi
-
-print('LOG_DIR=', getattr(orchestrator,'LOG_DIR',None))
-print('DB PATH=', getattr(orchestrator,'DB_PATH',None))
-print('DOWNLOAD ROOTS:', scraperapi.DB.list_download_locations())
